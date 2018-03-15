c782d0220c3c5239b4e28a8571aebf2a7c3443d4
ablanleuil <arthur.blanleuil@gmail.com>
2018-03-15 14:12:20 +0000

    Create library

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+use std::collections::HashMap;
+use std::convert::From;
+
+pub enum Shaun {
+    Null(),
+    Number(f64,String),
+    String(String),
+    Bool(bool),
+    List(Vec<Shaun>),
+    Object(HashMap<String,Shaun>),
+}
+
+impl From<String> for Shaun {
+    fn from(x:String) -> Self {
+        Shaun::String(x)
+    }
+}
+
+impl From<f64> for Shaun {
+    fn from(x:f64) -> Self {
+        Shaun::Number(x, String::new())
+    }
+}
+
+impl From<bool> for Shaun {
+    fn from(x:bool) -> Self {
+        Shaun::Bool(x)
+    }
+}
+
+impl<T> From<Vec<T>> for Shaun where Shaun : From<T> {
+    fn from(x:Vec<T>) -> Self {
+        Shaun::List(x.into_iter().map(|e| { Shaun::from(e) }).collect())
+    }
+}
+
+impl<T> From<HashMap<String,T>> for Shaun where Shaun : From<T> {
+    fn from(x:HashMap<String,T>) -> Self {
+        Shaun::Object(x.into_iter().map(|(k,v)| { (k, Shaun::from(v)) }).collect())
+    }
+}
